5d6882ac9afb4f481b4eb2ea8e593bdc56e57631
william8012 <william8012@naver.com>
2022-11-15 00:47:40 -0800

    added test & succes 1 sec delay

--- functions/src/controllers/api/index.ts
+++ functions/src/controllers/api/index.ts
@@ -2,6 +2,7 @@
 import admin from '@/plugins/firebase';
 import express from 'express';
 import { requestLog } from '@/utils/requestLog';
+import { sleep } from '@/utils/sleep';
 import * as functions from 'firebase-functions';
 import { statusCode } from '@/types/statusCode';
 const app = express();
@@ -14,12 +15,14 @@ app.use(cors({ origin: true })); // CORS
 app.get(
   '/test-success',
   async (req: express.Request, res: express.Response) => {
+    await sleep(1000);
     return res.send(true);
   }
 );
 
 app.get('/test-fail', async (req: express.Request, res: express.Response) => {
-  throw new Error(statusCode.BAD_REQUEST);
+  await sleep(1000);
+  throw Error(statusCode.BAD_REQUEST);
 });
 
 app.use(require('@/middlewares/errors'));
