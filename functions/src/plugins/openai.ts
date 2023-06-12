ede7656033fca96501fd96fd80325bdf4b6f997e
william8012 <william8012@naver.com>
2023-06-12 13:57:41 -0700

    open ai api added, plugins --functions: ai, user

---
+++ functions/src/plugins/openai.ts
@@ -0,0 +1,24 @@
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from 'openai';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+export interface IOpenAIMessage {
+  role: 'system' | 'assistant' | 'user';
+  content: string;
+}
+
+export async function sendPrompt(
+  model: 'gpt-3.5-turbo' | 'gpt-4',
+  messages: ChatCompletionRequestMessage[]
+) {
+  const completion = await openai.createChatCompletion({
+    model: model,
+    messages: messages,
+  });
+
+  return completion.data;
+}
